98027f226eb3b18f8f6e4dfc4e55b1ef68b161c2
YousefHusain2008 <172133198+YousefHusain2008@users.noreply.github.com>
2026-09-03 16:25:03 +0300

    first commit, bass c++ code with printing MAX_INV slots numbers

---
+++ testGitHubTraining/testGitHubTraining/src/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define newline "\n"
+#define logm(msg) cout << msg << newline;
+#define logv(var) cout << #var << " is: " << var << newline;
+
+using std::cout;
+using std::cin;
+
+// set max number of slots
+const short MAX_INV = 15;
+
+int main()
+{
+	logm("This is the beginning of the testGithub cpp file!");
+	logv(MAX_INV);
+	logm("This is the end of the testGithub cpp file!");
+	cin.get();
+	return 0;
+}
